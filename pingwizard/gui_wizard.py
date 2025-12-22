9eb48ab45c31eb13e27f560c605fca19f86b8235
Cameron Bodine <bodine.cs@gmail.com>
2025-12-22 03:38:08 -0500

    v1.0.19 try fix update

--- pingwizard/gui_wizard.py
+++ pingwizard/gui_wizard.py
@@ -142,13 +142,7 @@ def wizard():
                 f.write(f'''@echo off
 setlocal enabledelayedexpansion
 cd /d {conda_base}
-call {conda_base}\\Scripts\\conda.bat activate base
-if errorlevel 1 (
-    echo Failed to activate base environment
-    pause
-    exit /b 1
-)
-python -m pinginstaller
+"{conda_base}\\Scripts\\conda.bat" activate base && python -m pinginstaller
 pause
 ''')
                 batch_file = f.name
